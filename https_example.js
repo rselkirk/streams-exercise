bfa935651a70deabf18757cf16e59679eeefad36
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-04-09 22:17:55 +0000

    added incoming message

--- https_example.js
+++ https_example.js
@@ -6,8 +6,9 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
@@ -15,3 +16,5 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+
